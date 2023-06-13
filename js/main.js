f9c09bf3fb3dfafd6fb83087cf69d59a448b2bf6
kendrickPC <kennethpchang@gmail.com>
2023-06-14 03:17:32 +0800

    Random word generated from dictionary

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
 import { DICTIONARY } from "./dictionary.js";
 
-console.log(DICTIONARY.length)
+/* Global Constants*/
+const NUMBER_OF_GUESSES = 7
+
+/* Global Variables: */
+let guessesRemaining = NUMBER_OF_GUESSES
+const currentGuess = []
+let nextLetter = 0
+// Generates a random word from the dictionary and designates it as the answer word
+const answerWord = DICTIONARY[Math.floor(Math.random() * DICTIONARY.length)]
+
+
